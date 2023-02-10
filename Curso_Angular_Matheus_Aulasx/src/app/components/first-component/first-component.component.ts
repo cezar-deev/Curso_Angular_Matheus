c439110474d51b168bfa6273d2689af60a92503e
Cezar Cardoso <cezar.deev@gmail.com>
2023-02-10 19:12:02 -0300

    Initial commit

---
+++ Curso_Angular_Matheus_Aulasx/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent {
+
+  name: string = "Matheus";
+  age: number = 30;
+  job = "Programador";
+  hobbies = ["Cantar", "Passear"]
+  car = {
+    name: "Polo",
+    year: "2019"
+  }
+
+}
