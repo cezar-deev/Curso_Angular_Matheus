e65037301b1a214e2fd9802e6a43ad7978a974f4
Cezar Cardoso <cezar.deev@gmail.com>
2023-03-06 15:24:57 -0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'; // classe que criamos
 
 
 import { AppComponent } from './app.component';
@@ -30,7 +31,7 @@ import { TwoWayBindingComponent } from './components/two-way-binding/two-way-bin
     TwoWayBindingComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
