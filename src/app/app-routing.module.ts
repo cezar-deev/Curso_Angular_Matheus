8020644f40224c0fba2145eac9edc2101156ba02
Cezar Cardoso <cezar.deev@gmail.com>
2023-03-06 15:42:54 -0300

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { pipe } from "rxjs";
 
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from "./components/pipes/pipes.component";
 
 const routes: Routes = [
     {path:'', component: FirstComponentComponent},
-    {path:'list',component: ListRenderComponent}
+    {path:'list',component: ListRenderComponent},
+    {path:'pipes', component: PipesComponent}
 ]
 
 // É um decorator, que inclui nossas rotas nesta classe aqui
